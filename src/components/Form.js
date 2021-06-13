78e809f2b8dee98618949b3e545620d6cfa59a83
Martin Brueggemann <marty.brueggemann@Martins-MacBook-Pro.local>
2021-06-13 17:19:48 -0500

    Form Slices of State

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Response from './Response';
 
 const Form = () => {
+    const [ foodOrder, setFoodOrer ] = useState({
+        name: '',
+        size: '',
+        pepperoni: false,
+        sausage: false,
+        canadianBacon: false,
+        greenPepper: false,
+        dicedTomatoes: false,
+        roastedGarlic: false,
+        pineapple: false,
+        specialText: ''
+    })
+
+    const { name, size, pepperoni, sausage, canadianBacon, greenPepper, dicedTomatoes, roastedGarlic, pineapple, specialText } = foodOrder;
+
+    const changeHandler = event => {
+        setFoodOrer({...foodOrder, [event.target.name]: event.target.value});
+    }
+
     const submitHandler = event => {
         event.preventDefault();
-        console.log(event);
-    };
-
-    const onChangeHandler = event => {
-        console.log(event);
+        console.log(foodOrder);
     }
     return (
         <div>
@@ -15,11 +31,11 @@ const Form = () => {
             <form id='pizza-form' onSubmit={submitHandler}>
                 <label htmlFor='name'>
                     Name
-                    <input type='text' name='name' id='name-input' onChange={event => onChangeHandler(event.target.value)} />
+                    <input type='text' name='name' id='name-input' value={name} onChange={changeHandler} />
                     </label>
                 <label htmlFor='size'>
                     Choice of Size (required)
-                    <select name='size' id='size-dropdown' onChange={onChangeHandler}>
+                    <select name='size' id='size-dropdown' value={size} onChange={changeHandler}>
                         <option value ='small'>Personal Pizza (8")</option>
                         <option value ='med'>Regular (12")</option>
                         <option value ='large'>Large (14")</option>
@@ -29,42 +45,43 @@ const Form = () => {
                 <div id='toppings-container'>
                     <p>Add Toppings (pick up to 5)</p>
                     <label htmlFor='pepperoni'>
-                        <input type='checkbox' name='pepperoni' id='pepperoni' value='pepperoni' onChange={onChangeHandler}/>
+                        <input type='checkbox' name='pepperoni' id='pepperoni' value='pepperoni' onChange={changeHandler}/>
                         Pepperoni
                     </label>
                     <label htmlFor='sausage'>
-                        <input type='checkbox' name='sausage' id='sausage' value='sausage' onChange={onChangeHandler}/>
+                        <input type='checkbox' name='sausage' id='sausage' value='sausage' onChange={changeHandler}/>
                         Sausage
                     </label>
-                    <label htmlFor='canadian-bacon'>
-                        <input type='checkbox' name='canadian-bacon' id='canadian-bacon' value='canadian-bacon' onChange={onChangeHandler}/>
+                    <label htmlFor='canadianBacon'>
+                        <input type='checkbox' name='canadian-bacon' id='canadian-bacon' value='canadian-bacon' onChange={changeHandler}/>
                         Canadian Bacon
                     </label>
-                    <label htmlFor='green-pepper'>
-                        <input type='checkbox' name='green-pepper' id='green-pepper' value='green-pepper' onChange={onChangeHandler}/>
+                    <label htmlFor='greenPepper'>
+                        <input type='checkbox' name='greenPepper' id='greenPepper' value='greenPepper' onChange={changeHandler}/>
                         Green Pepper
                     </label>
-                    <label htmlFor='diced-tomatoes'>
-                        <input type='checkbox' name='diced-tomatoes' id='diced-tomatoes' value='diced-tomatoes' onChange={onChangeHandler}/>
+                    <label htmlFor='dicedTomatoes'>
+                        <input type='checkbox' name='dicedTomatoes' id='dicedTomatoes' value='dicedTomatoes' onChange={changeHandler}/>
                         Diced Tomatoes
                     </label>
-                    <label htmlFor='roasted-garlic'>
-                        <input type='checkbox' name='roasted-garlic' id='roasted-garlic' value='roasted-garlic' onChange={onChangeHandler}/>
+                    <label htmlFor='roastedGarlic'>
+                        <input type='checkbox' name='roastedGarlic' id='roastedGarlic' value='roastedGarlic' onChange={changeHandler}/>
                         Roasted Garlic
                     </label>
                     <label htmlFor='pineapple'>
-                        <input type='checkbox' name='pineapple' id='pineapple' value='pineapple' onChange={onChangeHandler}/>
+                        <input type='checkbox' name='pineapple' id='pineapple' value='pineapple' onChange={changeHandler}/>
                         Pineapple
                     </label>
                     <hr/>
-                    <label htmlFor='special-text'>
+                    <label htmlFor='specialText'>
                         Special Instructions
-                        <input type='textarea' name='special-text' if='special-text' placeholder='Anything else?' onChange={onChangeHandler}/>
+                        <input type='textarea' name='specialText' id='specialText' value='specialText' placeholder='Anything else?' onChange={changeHandler}/>
                     </label>
                     <hr/>
                     <button id='button' type='submit'>PLACE ORDER</button>
                 </div>
             </form>
+            <Response />
 
         </div>
     )
---
+++ src/components/Response.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Response = () => {
+    return (
+        <div>
+            <h2>Response</h2>
+        </div>
+    )
+}
+
+export default Response;
