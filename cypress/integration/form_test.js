73ce59008f521c2827b0b816dd28c77fbbc7ec45
Martin Brueggemann <marty.brueggemann@Martins-MacBook-Pro.local>
2021-06-13 19:59:26 -0500

    Cypress tests succesful

--- cypress/integration/form_test.js
+++ cypress/integration/form_test.js
@@ -5,3 +5,19 @@ describe('Text may be inputed', () => {
         .type('Jane Doe').should('have.value', 'Jane Doe');
     })
 })
+
+describe('Multiple toppings may be selected', () => {
+    it('can check multiple toppings', () => {
+        cy.visit('localhost:3000/pizza');
+        cy.get('[type="checkbox"]')
+        .check({force: true})
+        .should('be.checked')
+    })
+})
+
+describe('Order may be submitted', () => {
+    it('Posts submitted object', () => {
+        cy.visit('localhost:3000/pizza');
+        cy.get('#pizza-form').submit()
+    })
+})
